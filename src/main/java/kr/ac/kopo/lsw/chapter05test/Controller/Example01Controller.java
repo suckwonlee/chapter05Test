aa6c87d5a1d14f58f53da898f6de707045bbbc65
suckwonlee <lsw980912@gmail.com>
2025-04-09 11:30:01 +0900

    5장 요청 처리 메소드에서 사용하는 3자지 RequsetParam,PathVariable,MatrixVariable

---
+++ src/main/java/kr/ac/kopo/lsw/chapter05test/Controller/Example01Controller.java
@@ -0,0 +1,4 @@
+package kr.ac.kopo.lsw.chapter05test.Controller;
+
+public class Example01Controller {
+}
---
+++ src/test/java/kr/ac/kopo/lsw/chapter05test/Chapter05TestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.lsw.chapter05test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter05TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
